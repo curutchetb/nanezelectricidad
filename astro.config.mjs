5395d978eeca081d85e9b7160b0a62d148803609
curutchetb <77202352+curutchetb@users.noreply.github.com>
2026-04-09 08:10:18 -0300

    Actualizar astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://curutchetb.github.io', // Tu usuario
-  base: '/nanezelectricidad',         // El nombre de tu repo
+  //base: '/nanezelectricidad',         // El nombre de tu repo
   integrations: [tailwind()],
 });

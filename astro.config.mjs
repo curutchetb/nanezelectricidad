313aa3fa543fb3f8f3b2eba8bf49af38f2795d70
curutchetb <curutchetb@gmail.com>
2026-04-08 21:41:18 -0300

    Configurando Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://curutchetb.github.io', // Tu usuario
+  base: '/nanezelectricidad',         // El nombre de tu repo
   integrations: [tailwind()],
 });

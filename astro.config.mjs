aa9274b3af19921bd5a8993b343ca9a1cb398f4c
curutchetb <77202352+curutchetb@users.noreply.github.com>
2026-04-09 21:48:39 -0300

    Actualizar astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://nanezelectricidad.com.ar',
-  // Sin base: '/' para Vercel raíz
+  site: 'https://curutchetb.github.io',
 });
